83a2a291795f67421dbaf4b92ddb74b591960d8b
Subhashree-Sharma <sharmasubhashree88@gmail.com>
2025-01-07 15:44:05 +0530

    NewBranch commit

--- github.java
+++ github.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        
+        System.out.println("Addition code");
+        int a = 10;
+        int b = 20; 
+        int c = 10;
+        System.out.println(a+b+c);
+
     }
 }
